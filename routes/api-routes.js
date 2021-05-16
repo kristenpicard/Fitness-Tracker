41d869bc49f671da847ee766147043a41aa31796
Kristen Picard <kristenpicard93@gmail.com>
2021-05-16 13:16:08 -0400

    First get sort of working

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,56 +1,56 @@
 const router = require("express").Router();
-const Workout = require("../models");
+const Workout = require("../models/workout");
 
 // ACCORDING TO API.JS WILL NEED
 
 // GET RECENT WORKOUT
 
 router.get("/api/workouts", (req, res) => {
-  //   Transaction.find({})
-  //     .sort({ date: -1 })
-  //     .then((dbTransaction) => {
-  //       res.json(dbTransaction);
-  //     })
-  // .catch((err) => {
-  //   res.status(400).json(err);
-  // });
+  Workout.find({})
+    .sort({ date: -1 })
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
 });
 
 // PUT TO ADD AN EXCERCISE
 
-router.put("/api/workouts", ({ body }, res) => {
-  //   Transaction.insertMany(body)
-  //     .then((dbTransaction) => {
-  //       res.json(dbTransaction);
-  //     })
-  //     .catch((err) => {
-  //       res.status(400).json(err);
-  //     });
-});
+// router.put("/api/workouts", ({ body }, res) => {
+//   //   Transaction.insertMany(body)
+//   //     .then((dbTransaction) => {
+//   //       res.json(dbTransaction);
+//   //     })
+//   //     .catch((err) => {
+//   //       res.status(400).json(err);
+//   //     });
+// });
 
 // POST TO CREATE WORKOUT
 
-router.post("/api/workouts", ({ body }, res) => {
-  //   Transaction.create(body)
-  //     .then((dbTransaction) => {
-  //       res.json(dbTransaction);
-  //     })
-  //     .catch((err) => {
-  //       res.status(400).json(err);
-  //     });
-});
+// router.post("/api/workouts", ({ body }, res) => {
+//   //   Transaction.create(body)
+//   //     .then((dbTransaction) => {
+//   //       res.json(dbTransaction);
+//   //     })
+//   //     .catch((err) => {
+//   //       res.status(400).json(err);
+//   //     });
+// });
 
 // GET FOR WORKOUTS IN RANGE
 
-router.get("/api/workouts/range", (req, res) => {
-  //   Transaction.find({})
-  //     .sort({ date: -1 })
-  //     .then((dbTransaction) => {
-  //       res.json(dbTransaction);
-  //     })
-  // .catch((err) => {
-  //   res.status(400).json(err);
-  // });
-});
+// router.get("/api/workouts/range", (req, res) => {
+//   //   Transaction.find({})
+//   //     .sort({ date: -1 })
+//   //     .then((dbTransaction) => {
+//   //       res.json(dbTransaction);
+//   //     })
+//   // .catch((err) => {
+//   //   res.status(400).json(err);
+//   // });
+// });
 
 module.exports = router;
