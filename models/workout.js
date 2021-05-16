d86673c5e6adf4c2e81844871cbe15679c6e4a7d
Kristen Picard <kristenpicard93@gmail.com>
2021-05-16 11:48:53 -0400

    Working on model starter code

---
+++ models/workout.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+// K10 GRABBED FROM MINI PROJECT NEED TO UPDATE TO WORKOUT ITEMS
+const workoutSchema = new Schema({
+  name: {
+    type: String,
+    trim: true,
+    required: "Enter a name for transaction",
+  },
+  value: {
+    type: Number,
+    required: "Enter an amount",
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
