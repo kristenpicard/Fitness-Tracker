794be72361a1e812fd445dc66ea4be362ed1026e
Kristen Picard <kristenpicard93@gmail.com>
2021-05-16 12:01:59 -0400

    Got server running on localhost updated server and added html routes

--- server.js
+++ server.js
@@ -10,13 +10,14 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
+// routes
+require("./public/api");
+require("./routes/html-routes");
+
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
   useFindAndModify: false,
 });
-
-// routes K10 NEED TO ADD DEPENDING ON ROUTES MADE
-app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
